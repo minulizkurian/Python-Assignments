3c3e125ee88dba2253d6bcfac50ebfaa616f3a7c
Minu Liz Kurian <minulizkurian93@gmail.com>
2026-01-14 13:02:21 +0300

    Implement basic arithmetic operations in task1.py
    Added basic arithmetic operations: addition, subtraction, multiplication, and division.

--- Assignment-1/task1.py
+++ Assignment-1/task1.py
@@ -1,1 +1,10 @@
-
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: ")) 
+sum=a+b
+print("Addition: ",sum)
+Sub=a-b
+print("Subtraction: ",Sub)
+Mul=a*b
+print("Multiplication: ",Mul)
+Div=a/b
+print("Division ",Div)

d6c71d46ac4bb15fc15676b0098af638b7dd9a21
Minu Liz Kurian <minulizkurian93@gmail.com>
2026-01-19 13:18:08 +0300

    Add files via upload

---
+++ Assignment-2/Task1.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number:"))
+
+# If condition checks whether the num % 2 is zero. If so, it will be an even number else it will be an odd number.
+
+if num%2==0:
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is an odd number")
